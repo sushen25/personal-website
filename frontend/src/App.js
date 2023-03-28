f02bd3fe3b8cf0e15172ec8e9263f359ab68d2c8
Sushen Satturu <sushensatturu25@gmail.com>
2023-03-28 19:54:02 +1100

    Learning state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,37 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+function MyButton({count, handleClick}) {
+  return (
+    <button onClick={handleClick}>I have been clicked {count} times</button>
+  )
+}
+
+
+function Parent() {
+  const [count, setCount] = useState(0);
+  
+  function handleClick() {
+    setCount(count + 1)
+  }
+
+  return (
+    <>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} handleClick={handleClick} />
+      <MyButton count={count} handleClick={handleClick} />
+    </>
+  )
+
+}
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Parent />
       </header>
     </div>
   );
