a8ba019c13a68725ad9dd41ba84899625de959a9
Sushen Satturu <sushensatturu25@gmail.com>
2023-04-12 19:37:56 +1000

    Add react router and tailwind css

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
-import FilterableProductTable from './apiPractice/FilterableProductTable'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Suspense } from 'react'
 
 import './App.css'
 
 function App () {
     return (
         <div className="App">
-            <FilterableProductTable />
+            <Router>
+                {/* TODO - App Header */}
+                <Suspense fallback={''}>
+                    <Routes>
+                        <Route path="/" element={<h1 className="text-3xl font-bold underline">
+    Hello world!
+                        </h1>}/>
+
+                        <Route path="blogs" element={<h1>Blog</h1>}/>
+                    </Routes>
+                </Suspense>
+            </Router>
         </div>
     )
 }
