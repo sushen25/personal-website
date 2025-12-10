a7e23db1806f2da23ddce2a60a54e6ce311cce99
Sushen Satturu <sushensatturu25@gmail.com>
2025-12-11 08:21:24 +1100

    WEB-4 Add stream endpoint

--- agent-service/src/main.py
+++ agent-service/src/main.py
@@ -1,3 +1,4 @@
+import asyncio
 from agent.personal_assistant import create_personal_assistant
 
 from bedrock_agentcore.runtime import BedrockAgentCoreApp
@@ -5,21 +6,28 @@ from bedrock_agentcore.runtime import BedrockAgentCoreApp
 
 app = BedrockAgentCoreApp()
 
+# Sync endpoint
+# @app.entrypoint
+# def invoke(payload):
+#     """Process user input and return a response"""
+#     user_message = payload.get("prompt", "Hello")
+
+#     agent = create_personal_assistant()
+#     result = agent(user_message)
+
+#     return {"result": result.message}
 
 @app.entrypoint
-def invoke(payload):
-    """Process user input and return a response"""
-    print("PAYLOAD: ", payload)
-
+async def invoke(payload):
+    """Process user input and return a streaming response"""
     user_message = payload.get("prompt", "Hello")
 
-    print("User message")
-    print(user_message)
-
     agent = create_personal_assistant()
-    result = agent(user_message)
+    stream = agent.stream_async(user_message)
 
-    return {"result": result.message}
+    async for event in stream:
+        print(event)
+        yield event
 
 if __name__ == "__main__":
     app.run()
