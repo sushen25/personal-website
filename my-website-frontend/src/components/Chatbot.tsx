b18e63caae56f985cd9878daab81be1be434b2fc
Sushen Satturu <sushensatturu25@gmail.com>
2025-12-29 19:47:36 +1100

    WEB-4 Add new line on content block stop

--- my-website-frontend/src/components/Chatbot.tsx
+++ my-website-frontend/src/components/Chatbot.tsx
@@ -12,7 +12,7 @@ interface Message {
 }
 
 const normalizeMarkdown = (text: string) =>
-    text 
+    text
         // normalise Windows line endings
         .replace(/\r\n/g, '\n')
         // ensure headings start on a new line
@@ -173,6 +173,10 @@ export default function Chatbot() {
                             try {
                                 const event = JSON.parse(line);
 
+                                if (event.event?.contentBlockStop) {
+                                    accumulatedContent += " \n"
+                                }
+
                                 if (event.data) {
                                     accumulatedContent += event.data.split("\n").join(" \n");
 
@@ -236,8 +240,13 @@ export default function Chatbot() {
                         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
                     </svg>
                 ) : (
-                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
+                    <svg
+                        className="w-6 h-6"
+                        viewBox="0 0 24 24"
+                        fill="currentColor"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
                     </svg>
                 )}
             </button>
